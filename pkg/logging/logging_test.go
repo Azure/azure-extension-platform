9859b802afbc5901fb22c859d84316128d4da2e8
Bhaskar Brahma <bhbrahma@microsoft.com>
2021-01-26 12:05:29 -0800

    Added logging from stream (#9)

--- pkg/logging/logging_test.go
+++ pkg/logging/logging_test.go
@@ -1,9 +1,15 @@
 package logging
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
 	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/Azure/azure-extension-platform/pkg/constants"
 	"github.com/Azure/azure-extension-platform/pkg/handlerenv"
@@ -61,6 +67,98 @@ func Test_normalTrace(t *testing.T) {
 
 	dir, _ := ioutil.ReadDir(logtestdir)
 	require.Equal(t, 1, len(dir))
+}
+
+func Test_loggingFromStream(t *testing.T) {
+	he := getHandlerEnvironment(logtestdir)
+	el := New(he)
+
+	dateTimeRegexPattern := "[0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} "
+
+	errorRegexPattern := logLevelError + dateTimeRegexPattern
+	infoRegexPattern := logLevelInfo + dateTimeRegexPattern
+	warnRegexPattern := logLevelWarning + dateTimeRegexPattern
+
+	errorMessagePrefix := "error message prefix"
+	infoMessagePrefix := "info message prefix"
+	warnMessagePrefix := "warning message prefix"
+	errorMessage := "this is an error message"
+	infoMessage := "this is an info message"
+	warningMessage := "this is a warning message"
+
+	message1 := "message 1"
+	message2 := "message 2"
+
+	errorFilePath := path.Join(logtestdir, "errorfile")
+	errorFile, err := createFileAndWriteMessage(errorFilePath, errorMessage)
+	assert.NoError(t, err)
+
+	infoFilePath := path.Join(logtestdir, "infoFile")
+	infoFile, err := createFileAndWriteMessage(infoFilePath, infoMessage)
+	assert.NoError(t, err)
+
+	warnFilePath := path.Join(logtestdir, "warnFile")
+	warnFile, err := createFileAndWriteMessage(warnFilePath, warningMessage)
+	assert.NoError(t, err)
+
+	el.Error(message1)
+	el.ErrorFromStream(errorMessagePrefix, errorFile)
+	el.Error(message2)
+	el.InfoFromStream(infoMessagePrefix, infoFile)
+	el.WarnFromStream(warnMessagePrefix, warnFile)
+	errorFile.Close()
+	infoFile.Close()
+	warnFile.Close()
+
+	fileContents, err := readContentsOfMostRecentLogFile(logtestdir)
+	assert.NoError(t, err)
+	assert.Regexp(t, errorRegexPattern+message1, fileContents)
+	assert.Regexp(t, errorRegexPattern+errorMessagePrefix, fileContents)
+	assert.True(t, strings.Contains(fileContents, errorMessage))
+	assert.Regexp(t, errorRegexPattern+message2, fileContents)
+	assert.Regexp(t, infoRegexPattern+infoMessagePrefix, fileContents)
+	assert.True(t, strings.Contains(fileContents, infoMessage))
+	assert.Regexp(t, warnRegexPattern+warnMessagePrefix, fileContents)
+	assert.True(t, strings.Contains(fileContents, warningMessage))
+}
+
+func createFileAndWriteMessage(fileName, message string) (*os.File, error) {
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, constants.FilePermissions_UserOnly_ReadWrite)
+	if err != nil {
+		return file, err
+	}
+
+	file.Write([]byte(message))
+	file.Seek(0, io.SeekStart)
+	return file, err
+}
+
+func readContentsOfMostRecentLogFile(logfileDir string) (string, error) {
+	dir, err := ioutil.ReadDir(logfileDir)
+	if err != nil {
+		return "", err
+	}
+
+	var mostRecentLogFilename = ""
+	modTime, err := time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+	if err != nil {
+		return "", err
+	}
+	for _, fileInfo := range dir {
+		if !fileInfo.IsDir() && strings.HasPrefix(fileInfo.Name(), "log") {
+			if fileInfo.ModTime().After(modTime) {
+				modTime = fileInfo.ModTime()
+				mostRecentLogFilename = fileInfo.Name()
+			}
+		}
+	}
+	if mostRecentLogFilename == "" {
+		return "", fmt.Errorf("did not find log file under %s", logfileDir)
+	}
+	fileToRead := path.Join(logtestdir, mostRecentLogFilename)
+	fileContents, err := ioutil.ReadFile(fileToRead)
+	fileContentsString := string(fileContents)
+	return fileContentsString, nil
 }
 
 func getHandlerEnvironment(eventsFolder string) *handlerenv.HandlerEnvironment {
