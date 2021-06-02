c8f1c2f3e8532729e67c222c0249985c9d06b947
Moustafa Moustafa <momousta@microsoft.com>
2021-06-02 12:53:26 -0700

    Add missing imports, Add a unit test

--- pkg/seqno/seqno_linux_test.go
+++ pkg/seqno/seqno_linux_test.go
@@ -1,10 +1,12 @@
 package seqno
 
 import (
-	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"os"
 	"testing"
+
+	"github.com/Azure/azure-extension-platform/pkg/extensionerrors"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_writeReadSequenceNumberFile(t *testing.T) {
@@ -14,6 +16,13 @@ func Test_writeReadSequenceNumberFile(t *testing.T) {
 	assert.NoError(t, err, "set sequence number should succeed")
 	readSeqno, err := getSequenceNumberInternal("some name", "some version")
 	assert.NoError(t, err, "get sequence number should succeed")
+	assert.Equal(t, seqno, readSeqno, "read sequence number should be same as set sequence number")
+}
+
+func Test_ReadSequenceNumberNoMrseqFile(t *testing.T) {
+	seqno := uint(0)
+	readSeqno, err := getSequenceNumberInternal("some name", "some version")
+	assert.ErrorIs(t, err, extensionerrors.ErrNoMrseqFile)
 	assert.Equal(t, seqno, readSeqno, "read sequence number should be same as set sequence number")
 }
 
