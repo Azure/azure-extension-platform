7ea14a56c01303e23c0d38ab22485987148e7558
bhaskar brahma <bhaskarbrahma@outlook.com>
2021-10-07 15:26:47 -0700

    Removed extraneous status formatting outside of designated formatter

--- vmextension/enabledisable.go
+++ vmextension/enabledisable.go
@@ -66,7 +66,7 @@ func enable(ext *VMExtension) (string, error) {
 	msg, runErr := ext.exec.enableCallback(ext)
 	if runErr != nil {
 		ext.ExtensionLogger.Error("Enable failed: %v", runErr)
-		reportStatus(ext, status.StatusError, enableCmd, runErr.Error()+msg)
+		reportStatus(ext, status.StatusError, enableCmd, msg)
 	} else {
 		ext.ExtensionLogger.Info("Enable succeeded")
 		reportStatus(ext, status.StatusSuccess, enableCmd, msg)
@@ -105,7 +105,7 @@ func doesFileExistDisableDependency(filePath string) (bool, error) {
 
 func disable(ext *VMExtension) (string, error) {
 	disableCmd, exists := ext.exec.cmds["disable"]
-	if ! exists {
+	if !exists {
 		msg := "disable command not found"
 		ext.ExtensionLogger.Error(msg)
 		return msg, fmt.Errorf(msg)
@@ -119,7 +119,7 @@ func disable(ext *VMExtension) (string, error) {
 		} else {
 			err := setDisabled(ext, true)
 			if err != nil {
-				reportStatus(ext, status.StatusError, disableCmd, "disable failed " + err.Error())
+				reportStatus(ext, status.StatusError, disableCmd, "disable failed "+err.Error())
 				return "", err
 			}
 		}
@@ -132,7 +132,7 @@ func disable(ext *VMExtension) (string, error) {
 		err := ext.exec.disableCallback(ext)
 		if err != nil {
 			ext.ExtensionLogger.Error("Disable failed: %v", err)
-			reportStatus(ext, status.StatusError, disableCmd, "disable failed " + err.Error())
+			reportStatus(ext, status.StatusError, disableCmd, "disable failed "+err.Error())
 			return "", err
 		}
 	}
@@ -196,6 +196,5 @@ func setDisabled(ext *VMExtension, disabled bool) error {
 			return err
 		}
 	}
-
 	return nil
 }
