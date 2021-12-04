3e414f24023526b5ed8fcc0d7025b45196069096
Joseph Calev <jcalev@microsoft.com>
2021-12-03 16:27:52 -0800

    Actually allow the log file name to be specified (#21)

--- vmextension/initialization.go
+++ vmextension/initialization.go
@@ -27,6 +27,7 @@ type InitializationInfo struct {
 	InstallCallback       CallbackFunc                  // Called for the Install operation. Only set this if the extension wants a callback.
 	UninstallCallback     CallbackFunc                  // Called for the Uninstall operation. Only set this if the extension wants a callback.
 	CustomStatusFormatter status.StatusMessageFormatter // Provide a function to format the status message. If nil default formatting behavior will be preserved.
+	LogFileNamePattern    string                        // Default pattern to use for log files
 }
 
 // GetInitializationInfo returns a new InitializationInfo object
@@ -53,5 +54,6 @@ func GetInitializationInfo(name string, version string, requiresSeqNoChange bool
 		ResetStateCallback:  nil,
 		InstallCallback:     nil,
 		UninstallCallback:   nil,
+		LogFileNamePattern:  "",
 	}, nil
 }
