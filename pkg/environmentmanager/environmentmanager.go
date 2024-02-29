02acb24857dad6ccd2026177e877c268b7391b1c
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-02-29 00:26:22 -0800

    Refactor environment manager interface to expect ILogger

--- pkg/environmentmanager/environmentmanager.go
+++ pkg/environmentmanager/environmentmanager.go
@@ -12,8 +12,8 @@ import (
 // Allows for mocking all environment operations when running tests against VM extensions
 type IGetVMExtensionEnvironmentManager interface {
 	GetHandlerEnvironment(name string, version string) (*handlerenv.HandlerEnvironment, error)
-	FindSeqNum(el *logging.ExtensionLogger, configFolder string) (uint, error)
-	GetCurrentSequenceNumber(el *logging.ExtensionLogger, retriever seqno.ISequenceNumberRetriever, name, version string) (uint, error)
-	GetHandlerSettings(el *logging.ExtensionLogger, he *handlerenv.HandlerEnvironment) (*settings.HandlerSettings, error)
+	FindSeqNum(el logging.ILogger, configFolder string) (uint, error)
+	GetCurrentSequenceNumber(el logging.ILogger, retriever seqno.ISequenceNumberRetriever, name, version string) (uint, error)
+	GetHandlerSettings(el logging.ILogger, he *handlerenv.HandlerEnvironment) (*settings.HandlerSettings, error)
 	SetSequenceNumberInternal(extensionName, extensionVersion string, seqNo uint) error
 }
