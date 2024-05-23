06546520dd39cc7f2dc737c9b9462cbb3334872d
Mayank Daruka <mayankdarukamsft@gmail.com>
2024-05-23 15:44:29 -0500

    Fix test

--- pkg/commandhandler/cmd_test.go
+++ pkg/commandhandler/cmd_test.go
@@ -44,16 +44,3 @@ func TestStderr(t *testing.T) {
 	stdoutResult := strings.TrimSuffix(strings.TrimSuffix(string(fileBytes), lineReturnCharacter), " ")
 	assert.Equal(t, "1 2 3 4", stdoutResult)
 }
-
-func TestNonExistingCommand(t *testing.T) {
-	defer cleanupTest()
-	cmd := New()
-	retcode, err := cmd.Execute("command_does_not_exist", workingDir, workingDir, true, extensionLogger)
-	assert.Equal(t, commandNotExistReturnCode, retcode)
-	assert.Error(t, err, "command execution should fail")
-	assert.Contains(t, err.Error(), "is not recognized as an internal or external command", "error returned by cmd.Execute should include stderr")
-
-	fileInfo, err := os.ReadFile(path.Join(workingDir, "stderr"))
-	assert.NoError(t, err, "stderr file should be read")
-	assert.Contains(t, string(fileInfo), "is not recognized as an internal or external command", "stderr message should be as expected")
-}
